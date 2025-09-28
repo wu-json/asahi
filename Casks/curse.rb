2cfae07221a24f9935caf138e5585672633b5182
wu-json <jason.wu2153@gmail.com>
2025-09-28 02:16:57 -0700

    fix: checksum

--- Casks/curse.rb
+++ Casks/curse.rb
@@ -1,7 +1,7 @@
 cask "curse" do
   version "0.0.0"
-  sha256 arm:   "b73fe279dcf0a1552818b88c0d235226ebacaad31226a4d4691859f728b52946",
-         intel: "c7fcbe8bf49cb8ab28a7836b337461ab7034b236178a6ffd3125205edea3c6be"
+  sha256 arm:   "85905202061a0e596ba4dafe618d2bd34704b08ddf1005939e73bfb5cbc6a0bc",
+         intel: "4db112dd042588334345b798896b9ff7a2d10a231b20505dee73c10c9fd4b587"
 
   url "https://github.com/wu-json/curse/releases/download/v#{version}/curse_#{version}_darwin_#{Hardware::CPU.intel? ? "amd64" : "arm64"}.tar.gz"
   name "Curse"
